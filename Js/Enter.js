ff2bd380346e519e97d90105b1628fdb22823351
Bramuel <92329159+Bramuel@users.noreply.github.com>
2021-10-17 18:59:00 +0300

    Create Enter.js

---
+++ Js/Enter.js
@@ -0,0 +1,9 @@
+function enter(){
+  sum = document.getElementById('sum').value;
+  
+  if(sum == "Cal"){
+    document.location.href = "Calculator.html"
+  }else if(sum == "SA"){
+    document.location.href = "SA.html"
+  }
+}
