3fdf26e1a55d050269f74a5b2a200d0b9d3c1d60
Bramuel <92329159+Bramuel@users.noreply.github.com>
2021-12-04 13:51:52 +0300

    Update cube.js

--- Js/SA/cube.js
+++ Js/SA/cube.js
@@ -2,19 +2,20 @@ function go(){
   type = document.getElementById('select').value;
   unit = document.getElementById('unit').value;
   s = document.getElementById('side').value;
-
-  var ans = ""
   
   if(type == "open"){
     a = s * s;
     a = a * 5;
-    ans = a + unit + "²";
-    document.getElementById("Answer").innerHTML = ans;
+    document.getElementById('answer').innerHTML = a + unit + "²";
   }else if (type == "closed") {
-  
+    a = s * s;
+    a = a * 6;
+    document.getElementById('answer').innerHTML = a + unit + "²";
   } else if (type == "hollow") {
-  
+    a = s * s;
+    a = a * 4;
+    document.getElementById('answer').innerHTML = a + unit + "²";
   } else{
-    
+    document.getElementById('answer').innerHTML = "Error";
   }
 }
